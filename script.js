11e33bd4f316fbc4169b494c3bc7ebda62fe9074
KoNtRaSt0 <100592518+KoNtRaSt0@users.noreply.github.com>
2025-05-07 22:40:51 +0300

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 50) {
+        document.body.classList.add("scrolled");
+    } else {
+        document.body.classList.remove("scrolled");
+    }
+});
